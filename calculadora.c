e6e70618ab693f616614af50c0240ec6207a2a64
daviruy61 <165973026+daviruy61@users.noreply.github.com>
2024-08-30 18:48:40 -0300

    criaçao main

--- calculadora.c
+++ calculadora.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello world");
+    int escolha, numero;
+    float numero_real;
+
+    printf("Calculadora Programador Didática\n");
+    printf("Escolha a conversão desejada:\n");
+    printf("1 - Base 10 para Base 2\n");
+    printf("2 - Base 10 para Base 8\n");
+    printf("3 - Base 10 para Base 16\n");
+    printf("4 - Base 10 para Código BCD\n");
+    printf("5 - Base 10 para Complemento a 2 (16 bits)\n");
+    printf("6 - Real para Float e Double\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &escolha);
+
+    switch (escolha) {
+        case 1:
+            printf("Digite um número decimal: ");
+            scanf("%d", &numero);
+            base10_para_base2(numero);
+            break;
+        case 2:
+            printf("Digite um número decimal: ");
+            scanf("%d", &numero);
+            base10_para_base8(numero);
+            break;
+        case 3:
+            printf("Digite um número decimal: ");
+            scanf("%d", &numero);
+            base10_para_base16(numero);
+            break;
+        case 4:
+            printf("Digite um número decimal: ");
+            scanf("%d", &numero);
+            base10_para_bcd(numero);
+            break;
+        case 5:
+            printf("Digite um número decimal: ");
+            scanf("%d", &numero);
+            base10_para_complemento2(numero);
+            break;
+        case 6:
+            printf("Digite um número real: ");
+            scanf("%f", &numero_real);
+            real_para_float_double(numero_real);
+            break;
+        default:
+            printf("Opção inválida.\n");
+            break;
+    }
 
     return 0;
 }
